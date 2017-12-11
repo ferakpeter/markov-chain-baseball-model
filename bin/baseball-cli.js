e8150bbdcca1b5c3784a5b4ec472bbda503a8de9
fp <ferak.peter@gmail.com>
2017-12-11 11:41:26 +0100

    minor updates, wip, tests failing

--- bin/baseball-cli.js
+++ bin/baseball-cli.js
@@ -15,6 +15,12 @@ program
       var probabilities = baseball.runVector(userInput);
       console.log(probabilities);
       console.log("Sum: " + probabilities.reduce((a, b) => a + b) * 100 + "%");
+
+      var jagger = probabilities.map((i, x) => i * x);
+      console.log(jagger);
+      console.log(jagger.reduce((a, b) => a + b));
+
+
     } else if (command === 'prob') {
       var userInput = { runs: parseInt(program.runs), runners: parseInt(program.baserunners), outs: parseInt(program.outs)};
       console.log(userInput);
@@ -22,18 +28,19 @@ program
       var modified = probabilities.splice(0, parseInt(program.expected));
       console.log("Sum of expected:" + Math.round(probabilities.reduce((a, b) => a + b) * 10000) / 100 + "%");
     } else if (command === 'steal') {
-      var userInput = { runs: parseInt(program.runs), runners: parseInt(program.runners), outs: parseInt(program.outs)};
+      var userInput = { runs: parseInt(program.runs), runners: parseInt(program.baserunners), outs: parseInt(program.outs)};
       console.log(userInput);
-      var firstState = runProbabilities(baseball.runVector(userInput));
+      var e = program.expected != undefined ? parseInt(program.expected) : 1;
+      var firstState = runProbabilities(baseball.runVector(userInput), e);
       var inputRunners = (userInput.runners).toString().split('').map(Number);
       var firstRunner = inputRunners.splice(0, 1);
       var s2 = {runs: userInput.runs, runners: parseInt(firstRunner.map(i => i + 1).concat(inputRunners).join('')), outs: userInput.outs};
       console.log(s2);
-      var stolenState = runProbabilities(baseball.runVector(s2));
+      var stolenState = runProbabilities(baseball.runVector(s2), e);
       var i2 = (userInput.runners).toString().split('').map(Number);
       var s3 = {runs: userInput.runs, runners: Array.from(i2.slice(1, 3).join('')).map(Number).reduce((i, j) => i + j, 0), outs: userInput.outs + 1};
       console.log(s3);
-      var caughtState = runProbabilities(baseball.runVector(s3));
+      var caughtState = runProbabilities(baseball.runVector(s3), e);
 
       console.log(firstState);
       console.log(stolenState);
@@ -46,8 +53,8 @@ program
     }
   }).parse(process.argv);
 
-function runProbabilities(vector) {
-  noRun = vector.splice(0, 1);
+function runProbabilities(vector, expected) {
+  noRun = vector.splice(0, expected);
   run = vector.reduce((a, b) => a + b);
   return run;
 }
